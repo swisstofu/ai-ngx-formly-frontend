e882a71854cb6dc3e2d7c7aff6f5cf1237bceb36
swisstofu <swisstofu@gmail.com>
2025-10-31 11:47:31 +0100

    fix: update FormApiService import to use FormlyConfigService
    - Changed import from validator-registry.service to formly-config.service
    - ValidatorDefinition and ValidationMessageDefinition are now exported from FormlyConfigService
    - All 61 tests passing

--- src/app/services/form-api.service.ts
+++ src/app/services/form-api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { ValidatorDefinition, ValidationMessageDefinition } from './validator-registry.service';
+import { ValidatorDefinition, ValidationMessageDefinition } from './formly-config.service';
 
 /**
  * Response interface for form configuration
